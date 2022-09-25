616854e51cbf4492b38a5606fff314ae02ca0d08
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-09-25 17:56:45 -0500

    Toggle and Mood components added. Props and state in React. Resume about State is added

--- src/components/Codecademy/components/Toggle/Toggle.jsx
+++ src/components/Codecademy/components/Toggle/Toggle.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
-
-const green = '#39D1B4';
+ const green = '#39D1B4';
 const yellow = '#FFD712';
 
-export class Toggle extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={color:green};
-    this.changeColor = this.changeColor.bind(this);
+/* 
+  Proceso general de gestion del estado:
+
+  1.- Agregar el constructor 
+  2.- Inicializar el estado this.state={ name: 'Bulmaro'};
+  3.- this.handleEventFunction = this.handleEventFunction.bind(this);
+  4.- hadleEventFunction(){ 
+        Accedemos a la variable o propiedad del esttado a modificar con this.state.name
+      }
+  5.- Actualizamos el estado   this.setState({name: newName})
+  6.- En el render:
+      .- Accedemos al estado con this.state.name, 
+      .- Actualizamos el estado llamando a handleEventFunction() al dispararse un evento, onClick por ejemplo 
+*/
+ 
+export  class Toggle extends React.Component {
+
+  constructor (props){
+  super(props);
+  this.state= {color: yellow};
+  this.changeColor=this.changeColor.bind(this);
   }
 
+
   changeColor(){
-    console.log("oli");
-    const newColor = this.state.color == green ? yellow:green;
-    this.setState({color:newColor});
+    const newColor = this.state.color == yellow ? green : yellow;
+    this.setState({color: newColor});
   }
   render() {
     return (
-      <div style={{background: this.state.color}}>
-        <h1>
-          Change my color
-        </h1>
-        <button onClick={this.changeColor}>
-         Change color
-      </button>
-      </div>
-    );
+      <React.Fragment>
+        <div style={{ backgroundColor: this.state.color }}>
+          <button onClick={this.changeColor}>Mira como cambio de color</button>
+          </div> 
+      </React.Fragment>
+    )
   }
 }
-
